7d77cb85d68792add640d837dec3d2019fd094c9
WoutDeleu <wout.deleu@gmail.com>
2022-10-27 09:30:18 +0200

    Symmetric Encryption

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 //https://docs.oracle.com/javase/7/docs/technotes/guides/security/crypto/CryptoSpec.html
 
 import javax.crypto.*;
+import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class Main {
-    public static void main(String[] args) throws NoSuchAlgorithmException {
+    public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
         Person person1 = new Person("Harry", "Geluveld", "0496742300");
         Person person2 = new Person("Barry", "Marke", "049651524");
         Person person1Fake = new Person("Garry", "Geluveld", "0496742300");
@@ -14,9 +15,11 @@ public class Main {
         byte[] person1_byteArray = person1.getBytes();
         byte[] person2_byteArray = person2.getBytes();
         byte[] person1Fake_byteArray = person1Fake.getBytes();
+        System.out.println();
 
 
         // Hashing
+        System.out.println("************* HASHING *************");
         MessageDigest md = MessageDigest.getInstance("SHA-256");
         System.out.println("Original Person1");
         System.out.print(person1_byteArray);
@@ -37,8 +40,36 @@ public class Main {
 
 
         // Symmetric Encryption
+        System.out.println();
+        System.out.println("************* SYM ENCR *************");
         // AES: 128, 192, and 256 bits to encrypt and decrypt data in blocks of 128 bits
-//        KeyGenerator keygen = KeyGenerator.getInstance("SHA-256");
+        // AES: 512 -> 1024, (+64)
+        System.out.println("Original Text");
+        String textToEncrypt = "Franz Wilhelm Junghuhn (Mansfeld, 26 oktober 1809 – Lembang, 24 april 1864) was een Pruisisch-Nederlands ontdekkingsreiziger, indoloog, landmeter, arts, geograaf, geoloog en botanicus. Als onderzoeker bracht hij in dienst van de autoriteiten van Nederlands-Indië de geografie, geologie en natuur van Java en de Bataklanden op Sumatra in kaart. Naast het wetenschappelijk vastleggen van zijn ontdekkingen schreef Junghuhn ook boeken over zijn reizen, bekend vanwege hun gekleurde litho's van Indische landschappen.";
+        System.out.println(textToEncrypt);
+        System.out.println();
+
+        KeyGenerator keygen = KeyGenerator.getInstance("AES");
+        SecretKey skey = keygen.generateKey();
+        Cipher cipher = Cipher.getInstance("AES");
+
+        System.out.println("Encrypted Text");
+        cipher.init(Cipher.ENCRYPT_MODE, skey);
+        byte[] encryptedBytes = cipher.doFinal(textToEncrypt.getBytes());
+        String encryptedText = new String(encryptedBytes);
+        System.out.println(encryptedText);
+        System.out.println();
+
+        System.out.println("Decrypted Text");
+        cipher.init(Cipher.DECRYPT_MODE, skey);
+        byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
+        String decryptedText = new String(decryptedBytes);
+        System.out.println(decryptedText);
+
+
+        // Asymmetric Encryption
+        System.out.println();
+        System.out.println("************* ASYNC ENCR *************");
 
     }
 }
