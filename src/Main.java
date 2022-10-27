6b05bd7634d9a1c7339a3442e553f12c73fec2df
JonasPiscador <jonas.piscador@gmail.com>
2022-10-27 11:23:35 +0200

    digitalsign volledig

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import javax.crypto.*;
 import java.security.*;
 
 public class Main {
-    public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+    public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, SignatureException {
         Person person1 = new Person("Harry", "Geluveld", "0496742300");
         Person person2 = new Person("Barry", "Marke", "049651524");
         Person person1Fake = new Person("Garry", "Geluveld", "0496742300");
@@ -84,6 +84,20 @@ public class Main {
         String decryptedText_Asym = new String(asymDecryptedText);
         System.out.println("Decrypted Text");
         System.out.println(decryptedText_Asym);
+
+        System.out.println();
+        System.out.println("************* DIGITAL SIGN *************");
+        Signature signatureAlgorithm = Signature.getInstance("SHA256WithRSA");
+        signatureAlgorithm.initSign(keyPair.getPrivate());
+        signatureAlgorithm.update(textToEncrypt.getBytes());
+        byte[] signature = signatureAlgorithm.sign();
+
+        Signature verificationAlgorithm = Signature.getInstance("SHA256WithRSA");
+        verificationAlgorithm.initVerify(keyPair.getPublic());
+        verificationAlgorithm.update(textToEncrypt.getBytes());
+        boolean matches = verificationAlgorithm.verify(signature);
+        System.out.println("signature: "  + matches);
+
 
 
     }
