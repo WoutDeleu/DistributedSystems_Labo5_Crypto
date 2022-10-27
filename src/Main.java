b7304b9950e1e53dc3f8571cce3546b1d379b459
WoutDeleu <wout.deleu@gmail.com>
2022-10-27 07:52:25 +0200

    Goeiemorgen

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+//https://docs.oracle.com/javase/7/docs/technotes/guides/security/crypto/CryptoSpec.html
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person person1 = new Person("Harry", "Geluveld", "0496742300");
+        Person person2 = new Person("Barry", "Marke", "049651524");
+        Person person1Fake = new Person("Harry", "Geluveld", "0496742300");
     }
 }
